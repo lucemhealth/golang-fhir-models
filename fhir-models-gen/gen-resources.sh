1cc5b71369d1b6d7e8f184cf08e051c6020542e5
jhoward-lucem <93157110+jhoward-lucem@users.noreply.github.com>
2024-03-29 16:06:36 -0400

    LHP-6283 modified generator for polymorphic fhir elements. Changed Sa… (#2)

--- fhir-models-gen/gen-resources.sh
+++ fhir-models-gen/gen-resources.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-wget -O definitions.zip https://www.hl7.org/fhir/definitions.json.zip
+wget -O definitions.zip http://hl7.org/fhir/R4/definitions.json.zip
 unzip definitions.zip profiles-types.json valuesets.json -d fhir
 rm definitions.zip
-wget -O fhir/bundle.json http://hl7.org/fhir/bundle.profile.json
-wget -O fhir/codesystem.json http://hl7.org/fhir/codesystem.profile.json
-wget -O fhir/structuredefinition.json http://hl7.org/fhir/structuredefinition.profile.json
-wget -O fhir/valueset.json http://hl7.org/fhir/valueset.profile.json
+wget -O fhir/bundle.json http://hl7.org/fhir/R4/bundle.profile.json
+wget -O fhir/codesystem.json http://hl7.org/fhir/R4/codesystem.profile.json
+wget -O fhir/structuredefinition.json http://hl7.org/fhir/R4/structuredefinition.profile.json
+wget -O fhir/valueset.json http://hl7.org/fhir/R4/valueset.profile.json
 
 go generate ./fhir
